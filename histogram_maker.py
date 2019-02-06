263cd346df8126a1d8669fdb1b111a21633b8ca4
CodyDBrown <35937377+CodyDBrown@users.noreply.github.com>
2019-02-06 11:56:39 -0700

    Add files via upload
    Trying to clean up by github to make it a little more user friendly.

---
+++ histogram_maker.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def histogram_maker(*args, **kwargs):
+    """
+    Inputs
+    ----------
+    args: Should be the tables that we want to make histograms of
+    kwargs: Variables I want to make histograms of
+    """
