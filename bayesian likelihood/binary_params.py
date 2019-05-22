55a551277543c12a65252e8b5b5cbb06dd9c76f5
CodyDBrown <35937377+CodyDBrown@users.noreply.github.com>
2019-05-22 11:24:22 -0600

    Add files via upload
    Changed the r_peri threshold to be 5*r_star rather than 1.1*r_star

--- bayesian likelihood/binary_params.py
+++ bayesian likelihood/binary_params.py
@@ -2,10 +2,9 @@ import numpy as np
 import astropy.units as u
 from astropy.constants import G, sigma_sb, c
 
-
 def binary_params(m_min, mu, sigma, star_radius, star_mass):
     """
-    Find a set of parameters to use for making a binary orbit
+    Find a set of paramiters to use for making a binary orbit
 
     :param m_min: Minimum mass I want to consider (Jupyter Masses)
     :param mu: Mean value I want to use for the log normal period
@@ -18,11 +17,10 @@ def binary_params(m_min, mu, sigma, star_radius, star_mass):
 
     # Keep picking values until the distance to pericenter is large enough
     while r_peri < 5 * star_radius:
-        # Should this be mass fraction, rather than a set minimum mass value?
         m_buddy = np.random.uniform(m_min.value, star_mass.to(u.jupiterMass).value) * u.jupiterMass
 
         p_buddy = 10 ** np.random.normal(mu, sigma) *u.d
-        while p_buddy.value > 10**6: # Upper bound on the period. Should look into this a bit more.
+        while p_buddy.value > 10**6:
             p_buddy = 10 ** np.random.normal(mu, sigma) * u.d
 
         semi_major_axis = np.cbrt(((G * (star_mass + m_buddy)) / (4 * np.pi ** 2)) * p_buddy**2)
@@ -36,15 +34,8 @@ def binary_params(m_min, mu, sigma, star_radius, star_mass):
         r_peri = ((1 - eccentricity) * semi_major_axis).to(u.solRad)
         emergency += 1
         if emergency > 100:
-            p_buddy = 10 ** 6 *u.d
-            semi_major_axis = np.cbrt(((G * (star_mass + m_buddy)) / (4 * np.pi ** 2)) * p_buddy ** 2)
-            semi_major_axis = semi_major_axis.to(u.AU)  # Convert to AU
-            eccentricity = 0
-            r_peri = ((1 - eccentricity) * semi_major_axis).to(u.solRad)
-
-            if r_peri < 5*star_radius:
-                print("You got stuck")
-                break
+            print("You got stuck")
+            break
     n_holder = (2*np.pi) / p_buddy
 
     # Need to make 3 angle variabels
